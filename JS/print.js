90ec5606918033c926b55531caf60eceaa436e38
Kesav1316 <kesavram1316@gmail.com>
2023-09-23 11:59:48 +0530

    Added how to display js in html console Created functions Created conditions Created nested conditions Used console.log("") to print(js) Used alert("") to display message in pop up in browser(js)

---
+++ JS/print.js
@@ -0,0 +1,10 @@
+console.log("idkigspfmig") // To print
+
+
+
+function main() {
+    console.log("Hello"); //Prints in console 
+    alert("Hi ig "); //To show message in pop up
+}
+
+main()
---
+++ JS/printconditions.js
@@ -0,0 +1,20 @@
+function main1(grade) {
+    if (grade < 60) {
+        console.log("D");
+
+    } else if (grade < 70) {
+        console.log("C");
+
+    } else if (grade < 80) {
+        console.log("B");
+
+    } else if (grade < 90) {
+        console.log("A");
+    }
+    else {
+        console.log("AA")
+    }
+
+}
+
+main1(90)
