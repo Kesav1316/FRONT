1850c11d266d6c35fb82b16d6d32f295cef16424
Kesav1316 <kesavram1316@gmail.com>
2023-09-24 09:55:36 +0530

    Added while loop The variable used in condition of while loop should be declared before using the condition var i = 0; while (i<11) { console.log(i) }

--- JS/loops.js
+++ JS/loops.js
@@ -1,7 +1,8 @@
 console.log("LOOPING")
 
+//FOR LOOPING
 function main() {
-    for (let i = 0;i<11; i++) { // let - cannot be redeclared var - can be redeclared
+    for (var i = 0;i<11; i++) { // let - cannot be redeclared var - can be redeclared
         console.log(i); 
     }
     
@@ -9,3 +10,15 @@ function main() {
 
 
 main();
+
+//WHILE LOOPING
+function main1() {
+    var j = 0; //The variable should be declared before using while loop
+    while (j < 11) {
+        console.log(j);
+        j++;
+    }
+}
+
+
+main1();
--- JS/string.js
+++ JS/string.js
@@ -1,7 +1,7 @@
 s = "Hi ig";
 
 function main() {
-    for (var i of t) { //i of s becomes each item in string
+    for (var i of t) { //i of s - i becomes each item in string
         console.log(c);
     }
 
