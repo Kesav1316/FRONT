afe33315e43b538a2b1d6ecee266997d88382a76
Kesav1316 <kesavram1316@gmail.com>
2023-09-24 09:38:05 +0530

    ITERATING THROUGH ARRAYS USING LOOP CONDITIONS for ( i in l ) - i in l makes i index value and the index item can be printed using l[i] for ( i of l ) - i of l makes i the item

--- JS/array.js
+++ JS/array.js
@@ -1,10 +1,19 @@
 var l = [1,2,3,4,5,6];
 
 function main() {
-    for (var i in l) {
+    console.log("Taking the index value and iterating using l[i]");
+    for (var i in l) {  //i in l becomes index value and l[i] displays the array value in index i
+    
         console.log(l[i]); 
     }
 }
 
+function main1() {
+    console.log("Taking the value of array and iterating using i ")
+    for (var i of l) {  //i of l becomes the array value 
+        console.log(i);
+    }
+}
 
 main();
+main1();
